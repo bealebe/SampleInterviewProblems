329bff0954ecc6d82c808e8c99556fde228e5e05
bealebe <bryan.beale@gmail.com>
2017-10-12 16:46:17 -0500

    This is my solution to get the highest stock from yesterday's array of stocks...

--- AppleStocks/src/Main.java
+++ AppleStocks/src/Main.java
@@ -4,22 +4,31 @@
 public class Main {
     public static void main(String[] args){
 
-        int[] stockPricesYesterday = new int[] {10, 7, 5, 8, 11, 9};
+        int[] stockPricesYesterday = new int[] {-1, -2};
 
         System.out.println(getMaxProfit(stockPricesYesterday));
 
     }
 
     public static int getMaxProfit(int[] pStockPricesYesterday){
-        int maxPrice = 0;
+        if( pStockPricesYesterday.length <2){
+            throw new IllegalArgumentException("Getting a max profit requires two numbers at least...");
+        }
+        int maxProfit = pStockPricesYesterday[1] - pStockPricesYesterday[0];
+        int minPrice = pStockPricesYesterday[0];
 
-        for(int i : pStockPricesYesterday){
-            if (i > maxPrice){
-                maxPrice = i;
-            }
+        for(int i = 1; i <  pStockPricesYesterday.length; i++){
+
+            int current =  pStockPricesYesterday[i];
+
+            int potentialProfit = current - minPrice;
+
+            maxProfit = Math.max(potentialProfit, maxProfit);
+
+            minPrice = Math.min(current, minPrice);
         }
 
-        return maxPrice;
+        return maxProfit;
     }
 
 
