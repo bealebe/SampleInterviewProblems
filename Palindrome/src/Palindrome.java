98f3fb6e49a79a8bb090261b90503e02c12caa0b
Bryan E Beale <bryan.beale@gmail.com>
2017-10-19 22:45:13 -0500

    Changed to one Liner

--- Palindrome/src/Palindrome.java
+++ Palindrome/src/Palindrome.java
@@ -3,14 +3,7 @@
  */
 public class Palindrome {
     public static boolean isPalindrome(String word) {
-        String wordReversed = "";
-
-        for(int i=word.length()-1; i>=0; i--){
-            wordReversed += word.charAt(i);
-        }
-
-        return wordReversed.equalsIgnoreCase(word);
-
+        return word.equalsIgnoreCase(new StringBuilder(word).reverse().toString());
     }
 
     public static void main(String[] args) {
